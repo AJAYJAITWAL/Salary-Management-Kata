559a5c48c9e8cc149e3015365fddebbb2c9c1ee8
AJAYJAITWAL <ajayjaitwal3434@gmail.com>
2026-02-25 18:37:38 +0530

    extract private methods and improve clarity

--- app/services/payroll/salary_calculator.rb
+++ app/services/payroll/salary_calculator.rb
@@ -10,21 +10,35 @@ module Payroll
     end
 
     def call
-      gross = employee.salary
-      rate = DEDUCTION_RULES.fetch(employee.country.to_s.downcase, BigDecimal("0"))
-
-      tds = (gross * rate).round(2)
-      net = (gross - tds).round(2)
-
       {
-        gross: gross,
-        tds: tds,
-        net: net
+        gross: gross_salary,
+        tds: tds_amount,
+        net: net_salary
       }
     end
 
     private
 
     attr_reader :employee
+
+    def gross_salary
+      employee.salary
+    end
+
+    def deduction_rate
+      DEDUCTION_RULES.fetch(normalized_country, BigDecimal("0"))
+    end
+
+    def normalized_country
+      employee.country.to_s.strip.downcase
+    end
+
+    def tds_amount
+      (gross_salary * deduction_rate).round(2)
+    end
+
+    def net_salary
+      (gross_salary - tds_amount).round(2)
+    end
   end
 end
