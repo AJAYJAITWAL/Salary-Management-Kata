dae6141be7bc93adb5bd8c207f3617ec4b5bf3cf
AJAYJAITWAL <ajayjaitwal3434@gmail.com>
2026-02-25 18:28:36 +0530

    implement employee CRUD endpoints

---
+++ app/controllers/api/v1/employees_controller.rb
@@ -0,0 +1,40 @@
+module Api
+  module V1
+    class EmployeesController < ApplicationController
+
+      def index
+        employees = Employee.all
+        render json: employees, status: :ok
+      end
+
+      def show
+        @employee = Employee.find(params[:id])
+        render json: @employee, status: :ok
+      end
+
+      def create
+        employee = Employee.create!(employee_params)
+        render json: employee, status: :created
+      end
+
+      def update
+        @employee = Employee.find(params[:id])
+        @employee.update!(employee_params)
+        render json: @employee, status: :ok
+      end
+
+      def destroy
+        @employee = Employee.find(params[:id])
+        @employee.destroy!
+        head :no_content
+      end
+
+      private
+
+      def employee_params
+        params.require(:employee)
+              .permit(:full_name, :job_title, :country, :salary)
+      end
+    end
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound do
+    render json: { error: "Not Found" }, status: :not_found
+  end
+
+  rescue_from ActiveRecord::RecordInvalid do |exception|
+    render json: { errors: exception.record.errors.full_messages },
+           status: :unprocessable_entity
+  end
 end
